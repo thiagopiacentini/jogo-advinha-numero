2383138a8a693cb79c4ae130159a24c852626462
Thiago Piacentini <37667825+thiagopiacentini@users.noreply.github.com>
2022-06-10 22:52:58 -0300

    publicando o game

---
+++ app/validacao.js
@@ -0,0 +1,55 @@
+function verificaSeOChutePossuiValorValido(chute) {
+  const numero = +chute;
+
+  if (chuteForInvalido(numero)) {
+    if (chute.toUpperCase() === 'FINALIZAR') {
+      document.body.innerHTML = `
+        <h2>Game Over!!!</h2>
+        <h3>Pressione o botão para jogar novamente</h3>
+        <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+      `;
+      document.body.style.backgroundColor = "#5f5f5f";
+
+    } else {
+      elementoChute.innerHTML += '<div>Valor inválido!</div>';
+      return;
+    }
+  }
+
+  if (numeroMaiorOuMenorQueOValorPermitido(numero)) {
+    elementoChute.innerHTML += `<div>Fale um número entre ${menorValor} e ${maiorValor}</div>`;
+    return;
+  }
+
+  if (numero === numeroSecreto) {
+    document.body.innerHTML = `
+      <h2>Você acertou!</h2>
+      <h3>O número secreto era ${numeroSecreto}</h3>
+      <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+    `;
+  } else if (numero > numeroSecreto) {
+    elementoChute.innerHTML += `
+      <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+    `;
+  } else {
+    elementoChute.innerHTML += `
+      <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+    `;
+  }
+
+}
+
+
+function chuteForInvalido(numero) {
+  return Number.isNaN(numero);
+}
+
+function numeroMaiorOuMenorQueOValorPermitido(numero) {
+  return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener('click', (e) => {
+  if (e.target.id === 'jogar-novamente') {
+    window.location.reload();
+  }
+});
